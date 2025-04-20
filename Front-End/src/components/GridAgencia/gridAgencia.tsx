448024c5822a6aa70935f9e113bc1c46a3fdae8b
Pedro Nery <pedrohnery@gmail.com>
2025-04-19 21:35:34 -0300

    Create Grids suspeitos

---
+++ Front-End/src/components/GridAgencia/gridAgencia.tsx
@@ -0,0 +1,36 @@
+import { Table } from "react-bootstrap";
+import { IAgenciaSuspeitas } from "../../Interfaces/IAgenciasSuspeitas";
+
+
+interface PropsData{
+    data?:IAgenciaSuspeitas[],
+}
+
+export default function GridAgencia({data}:PropsData) {
+    return (
+        <Table className="table table-bordered">
+            <thead>
+                <tr>
+                    <td  className="tg-0pky">Banco</td>
+                    <td  className="tg-0pky">Agência</td>
+                    <td  className="tg-0pky">Valor Movimentado</td>
+                    <td  className="tg-0pky">Tipo movimentação</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                   data?.map((data,index)=>(
+                        <tr key={index}>
+
+                            <th>{data.banco}</th>
+                            <th>{data.agencia}</th>
+                            <th >{Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(data.valor)} </th>
+                            <th>{data.operacao}</th>
+                                
+                        </tr>
+                   ))
+                }
+            </tbody>
+        </Table>
+    )
+}
---
+++ Front-End/src/components/GridConta/gridConta.tsx
@@ -0,0 +1,38 @@
+import { Table } from "react-bootstrap";
+import { IContasSuspeitas } from "../../Interfaces/IContasSuspeita";
+
+
+interface PropsData{
+    data?:IContasSuspeitas[],
+}
+
+export default function GridContaAgencia({data}:PropsData) {
+    return (
+        <Table className="table table-bordered">
+            <thead>
+                <tr>
+                    <td  className="tg-0pky">Banco</td>
+                    <td  className="tg-0pky">Agência</td>
+                    <td  className="tg-0pky">Conta</td>
+                    <td  className="tg-0pky">Valor Movimentado</td>
+                    <td  className="tg-0pky">Tipo movimentação</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                   data?.map((data,index)=>(
+                        <tr key={index}>
+
+                            <th>{data.banco}</th>
+                            <th>{data.agencia}</th>
+                            <th>{data.conta}</th>
+                            <th >{Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(data.valor)} </th>
+                            <th>{data.operacao}</th>
+                                
+                        </tr>
+                   ))
+                }
+            </tbody>
+        </Table>
+    )
+}
